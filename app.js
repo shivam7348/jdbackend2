249db75566afc1d1e24034d60bae664d3eeeed06
Pradeep8253 <89512667+Pradeep8253@users.noreply.github.com>
2025-02-28 08:08:27 +0530

    backend

--- app.js
+++ app.js
@@ -12,33 +12,20 @@ import annualPlannerRoutes from "./routers/annualPlannerRoutes.js";
 dotenv.config();
 
 const app = express();
-const __dirname = path.resolve();
 
-// Middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://your-deployed-frontend-url.com"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 // Connect to Database
 connectDB();
 
 // Routes
-app.use("/api/admin/users", adminUserRoutes);
+app.use("/api/admin", adminUserRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/activity", activityRoutes);
 app.use("/api/gallery", galleryRoutes);
 app.use("/api/annualPlanner", annualPlannerRoutes);
-
-// Serve frontend
-app.use(express.static(path.join(__dirname, "client/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
-});
 
 // Start Server
 const PORT = process.env.PORT || 5000;
